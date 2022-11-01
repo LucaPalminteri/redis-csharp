e615246b0766112d4ea1fc421ff61254f2da1ede
LucaPalminteri <lucapalminteri02@gmail.com>
2022-11-01 07:45:57 -0300

    first commit

---
+++ redis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using StackExchange.Redis;
+
+
+namespace redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redisDB = RedisDB.Connection.GetDatabase();
+
+            redisDB.StringSet("1", "Luca");
+
+            var valor = redisDB.StringGet("1");
+
+            Console.WriteLine(valor);
+        }
+    }
+}
---
+++ redis/RedisDB.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using StackExchange.Redis;
+
+namespace redis
+{
+    public class RedisDB
+    {
+        private static Lazy<ConnectionMultiplexer> _lazyConnection;
+
+        public static ConnectionMultiplexer Connection
+        {
+            get { return _lazyConnection.Value; }
+        }
+        static RedisDB()
+        {
+            _lazyConnection = new Lazy<ConnectionMultiplexer>(() =>
+                ConnectionMultiplexer.Connect("localhost")
+            );
+        }
+    }
+}
